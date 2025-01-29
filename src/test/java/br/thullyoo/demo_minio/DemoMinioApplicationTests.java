746196b589ecf4586f730146c4dfe66bab3cee6f
Thullyoo <thullyofilho@gmail.com>
2025-01-29 11:41:06 -0300

    Initial commit

---
+++ src/test/java/br/thullyoo/demo_minio/DemoMinioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.demo_minio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMinioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
